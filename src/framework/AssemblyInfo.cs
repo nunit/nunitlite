ca85c4b4700f9654d923d9635366ce152146020c
Charlie Poole <charlie@nunit.org>
2012-09-24 11:42:32 -0700

    Upgrade NAnt to version 0.92 and advance NUnitLite version to 0.9

--- src/framework/AssemblyInfo.cs
+++ src/framework/AssemblyInfo.cs
@@ -100,7 +100,7 @@ using System;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
 #if !PocketPC && !WindowsCE && !NETCF
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
 #endif
